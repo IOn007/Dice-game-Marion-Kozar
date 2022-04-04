938162bddcdc4b774016144cb683c1bc5c5570e3
Marion Kozar <marion.kozar@hotmail.fr>
2022-04-04 18:53:54 +0200

    on réinitialise le round score et  switch de joueur à chaque fois que le 1 tombe

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let roundPlayer1 = 0;
 let roundPlayer2 = 0;
 let globalPlayer1 = 0;
 let globalPlayer2 = 0; 
+let currentPlayer = 1;
 
 function handleNewGame(event) {  
   // A chaque nouvelle partie, round et global sont réinitialisés à 0
@@ -36,17 +37,60 @@ function handleRollDice(event) {
   if (event.currentTarget.id === 'roll') {
     // peut lancer le dé 
     let dice = Number(rollDice(1,6));
-    console.log(dice);
+    console.log("ceci est dé" + dice);
 
     // on affiche l'image du dé correspondant au tirage au sort
     let diceImage = document.getElementById('dice');
     diceImage.src = "img/dice" + dice + ".svg";
     console.log(diceImage.src);
-
-    //on incrémente le score à round player 1
-    roundPlayer1 += dice;
-    console.log(roundPlayer1);
-    document.getElementById('roundPlayer1').innerHTML = roundPlayer1;
+    // Si dice = 1
+    if (dice === 1){
+      switch (currentPlayer) {
+        case 1:
+          // on vide le score round
+          roundPlayer1 = 0;
+          document.getElementById('roundPlayer1').innerHTML = roundPlayer1;
+          // on passe la main au joueur 2
+          // on retire la classe active sur le joueur 1 qui joue en premier
+          document.getElementById('player1').className = '';
+          // on ajoute la classe active sur le joueur 2 qui joue en premier
+          document.getElementById('player2').className = 'active';
+          break;
+        case 2:
+          // on vide le score round
+          roundPlayer2 = 0;
+          document.getElementById('roundPlayer2').innerHTML = roundPlayer2;
+          // on passe la main au joueur 2
+          // on retire la classe active sur le joueur 1 qui joue en premier
+          document.getElementById('player2').className = '';
+          // on ajoute la classe active sur le joueur 2 qui joue en premier
+          document.getElementById('player1').className = 'active';
+          break;
+      }
+      // On change de joueur
+      if (currentPlayer === 1) {
+        currentPlayer = 2;
+      } else {
+        currentPlayer = 1;
+      }
+      console.log("joueur qui joue : " + currentPlayer);
+    }
+    else {
+      switch (currentPlayer) {
+        case 1:
+          //on incrémente le score à round player 1
+          roundPlayer1 += dice;
+          console.log("ceci est le score incrémenté du joueur 1: " + roundPlayer1);
+          document.getElementById('roundPlayer1').innerHTML = roundPlayer1;
+          break;
+        case 2:
+          //on incrémente le score à round player 2
+          roundPlayer2 += dice;
+          console.log("ceci est le score incrémenté du joueur 2: " + roundPlayer2);
+          document.getElementById('roundPlayer2').innerHTML = roundPlayer2;
+          break;
+      }
+    }
   }
 }
 
@@ -54,8 +98,7 @@ document.getElementById('newGame').addEventListener('click', handleNewGame);
 document.getElementById('roll').addEventListener('click', handleRollDice);
 
 
-  // si le dé = 1 alors round =0 et fin du tour
-  // si dé >1 alors incrémente round avec la valeur du dé
+
 
 // peut ajouter le score de round à global
   // et passe la main
